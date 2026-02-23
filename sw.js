19ca88cda70765a9d25c6ab24e75d49c09af91be
pere347 <pere.llorens.lluis@gmail.com>
2026-02-23 10:34:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,27 @@
-// Aquest és el nom de la nostra memòria cau (Cache)
-const CACHE_NAME = 'upan-cache-v1';
+const CACHE_NAME = 'upan-cache-v2'; // He pujat la versió a v2 per forçar l'actualització
 
-// Llista d'arxius que volem guardar al mòbil per quan no hi hagi internet
 const urlsToCache = [
-  './',               // La pàgina principal
-  './index.html'      // L'arxiu HTML
+  './',
+  './index.html',
+  // Afegim Leaflet a la memòria cau!
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
 
-// 1. FASE D'INSTAL·LACIÓ: Guardem els arxius a la Cache
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Arxius guardats a la memòria cau correctament');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// 2. FASE DE LECTURA (INTERCEPTOR): Què fem quan l'usuari refresca la pàgina?
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Si l'arxiu està a la Cache (fins i tot sense internet), el retornem!
-        if (response) {
-          return response;
-        }
-        // Si no hi és, anem a internet a buscar-lo
+        if (response) return response;
         return fetch(event.request);
       })
   );
